fc8e6d28670a8218592a5f24abf6eb8e30638a89
Pavel Ponomarev <pavponn@yandex.ru>
2018-12-17 12:03:04 +0300

    Suffix Array added

---
+++ String/SuffixArray.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <fstream>
+
+std::string str;
+const size_t MAX_STRING_LENGTH = 400000 + 50;
+const size_t MAX_ALPHABET_SIZE = 160;
+std::vector<int> pos(MAX_STRING_LENGTH);
+std::vector<int> lcp(MAX_STRING_LENGTH);
+std::vector<int> mSuffArray(MAX_STRING_LENGTH);
+std::vector<int> mCnt(MAX_STRING_LENGTH);
+std::vector<int> mClasses(MAX_STRING_LENGTH);
+
+int getCharacterCode(char ch) {
+    return ch - '$';
+}
+
+void buildArray(std::string str) {
+
+    std::vector<int> suffArrayTemp;
+    std::vector<int> classesTemp;
+
+    suffArrayTemp.resize(MAX_STRING_LENGTH);
+    classesTemp.resize(MAX_STRING_LENGTH);
+
+    // Zero phase
+
+    for (size_t i = 0; i < str.size(); ++i) {
+        ++mCnt[getCharacterCode(str[i])];
+    }
+    for (size_t i = 1; i < MAX_ALPHABET_SIZE; ++i) {
+        mCnt[i] += mCnt[i - 1];
+    }
+    for (int i = 0; i < str.size(); ++i) {
+        mSuffArray[--mCnt[getCharacterCode(str[i])]] = i;
+    }
+    mClasses[mSuffArray[0]] = 0;
+    int count = 1;
+    for (size_t i = 1; i < str.size(); ++i) {
+        if (str[mSuffArray[i - 1]] != str[mSuffArray[i]]) {
+            ++count;
+        }
+        mClasses[mSuffArray[i]] = count - 1;
+    }
+
+    //Other phases
+
+    for (int k = 0; (1 << k) < str.size(); ++k) {
+        int cur = 1 << k;
+        for (int i = 0; i < str.size(); ++i) {
+            suffArrayTemp[i] = mSuffArray[i] - cur;
+            if (suffArrayTemp[i] < 0) {
+                suffArrayTemp[i] += str.size();
+            }
+        }
+        mCnt.resize(static_cast<unsigned long>(count));
+        std::fill(mCnt.begin(), mCnt.end(), 0);
+        for (int i = 0; i < str.size(); ++i) {
+            ++mCnt[mClasses[suffArrayTemp[i]]];
+        }
+        for (int i = 1; i < count; ++i) {
+            mCnt[i] += mCnt[i - 1];
+        }
+        for (auto i = static_cast<int>(str.size() - 1); i >= 0; --i) {
+            mSuffArray[--mCnt[mClasses[suffArrayTemp[i]]]] = suffArrayTemp[i];
+        }
+        classesTemp[mSuffArray[0]] = 0;
+        count = 1;
+        for (int i = 1; i < str.size(); ++i) {
+            size_t mid1 = (mSuffArray[i] + cur) % str.size();
+            size_t mid2 = (mSuffArray[i - 1] + cur) % str.size();
+            if (mClasses[mSuffArray[i]] != mClasses[mSuffArray[i - 1]] || mClasses[mid1] != mClasses[mid2]) {
+                ++count;
+            }
+            classesTemp[mSuffArray[i]] = count - 1;
+        }
+
+        mClasses = classesTemp;
+
+
+    }
+}
+
+std::vector<int> calculateLCP() {
+    for (int i = 0; i < str.size(); ++i) {
+        pos[mSuffArray[i]] = i;
+    }
+    int k = 0;
+    for (int i = 0; i < str.size(); ++i) {
+        if (k > 0) {
+            --k;
+        }
+        if (pos[i] == static_cast<int>(str.size() - 1)) {
+            lcp[str.size() - 1] = -1;
+            k = 0;
+        } else {
+            int cur = mSuffArray[pos[i] + 1];
+            while (std::max(i + k, cur + k) < str.size() &&
+                   str[i + k] == str[cur + k]) {
+                ++k;
+            }
+            lcp[pos[i]] = k;
+        }
+
+    }
+    return lcp;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    std::ifstream in("array.in");
+    std::ofstream out("array.out");
+
+    in >> str;
+    str += '$';
+    buildArray(str);
+    for (int i = 1; i < str.length(); ++i) {
+        out << mSuffArray[i] + 1 << " ";
+    }
+    out << std::endl;
+    std::vector<int> lcp = calculateLCP();
+    for (int i = 1; i < str.length() - 1; ++i) {
+        out << lcp[i] << " ";
+    }
+
+    return 0;
+}
---
+++ String/SuffixArrayClass.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <fstream>
+
+std::string str;
+std::vector<int> pos(400000 + 50);
+std::vector<int> lcp(400000 + 50);
+
+class SuffixArray {
+public:
+    explicit SuffixArray(std::string& str) {
+        mSize = str.size();
+        mString = str;
+        mSuffArray.resize(MAX_STRING_LENGTH);
+        mClasses.resize(MAX_STRING_LENGTH);
+        mCnt.resize(MAX_STRING_LENGTH);
+        buildArray(str);
+    }
+
+    int operator[](size_t i) {
+        return mSuffArray[i];
+    }
+
+    size_t getSize() {
+        return mSize;
+    }
+
+    std::string getString() {
+        return mString;
+    }
+
+private:
+
+    void buildArray(std::string str) {
+
+        std::vector<int> suffArrayTemp;
+        std::vector<int> classesTemp;
+
+        suffArrayTemp.resize(MAX_STRING_LENGTH);
+        classesTemp.resize(MAX_STRING_LENGTH);
+
+        // Zero phase
+
+        for (size_t i = 0; i < mSize; ++i) {
+            ++mCnt[getCharacterCode(str[i])];
+        }
+        for (size_t i = 1; i < MAX_ALPHABET_SIZE; ++i) {
+            mCnt[i] += mCnt[i - 1];
+        }
+        for (int i = 0; i < mSize; ++i) {
+            mSuffArray[--mCnt[getCharacterCode(str[i])]] = i;
+        }
+        mClasses[mSuffArray[0]] = 0;
+        int count = 1;
+        for (size_t i = 1; i < mSize; ++i) {
+            if (str[mSuffArray[i - 1]] != str[mSuffArray[i]]) {
+                ++count;
+            }
+            mClasses[mSuffArray[i]] = count - 1;
+        }
+
+        //Other phases
+
+        for (int k = 0; (1 << k) < mSize; ++k) {
+            int cur = 1 << k;
+            for (int i = 0; i < mSize; ++i) {
+                suffArrayTemp[i] = mSuffArray[i] - cur;
+                if (suffArrayTemp[i] < 0) {
+                    suffArrayTemp[i] += mSize;
+                }
+            }
+            mCnt.resize(static_cast<unsigned long>(count));
+            std::fill(mCnt.begin(), mCnt.end(), 0);
+            for (int i = 0; i < mSize; ++i) {
+                ++mCnt[mClasses[suffArrayTemp[i]]];
+            }
+            for (int i = 1; i < count; ++i) {
+                mCnt[i] += mCnt[i - 1];
+            }
+            for (auto i = static_cast<int>(mSize - 1); i >= 0; --i) {
+                mSuffArray[--mCnt[mClasses[suffArrayTemp[i]]]] = suffArrayTemp[i];
+            }
+            classesTemp[mSuffArray[0]] = 0;
+            count = 1;
+            for (int i = 1; i < mSize; ++i) {
+                size_t mid1 = (mSuffArray[i] + cur) % mSize;
+                size_t mid2 = (mSuffArray[i - 1] + cur) % mSize;
+                if (mClasses[mSuffArray[i]] != mClasses[mSuffArray[i - 1]] || mClasses[mid1] != mClasses[mid2]) {
+                    ++count;
+                }
+                classesTemp[mSuffArray[i]] = count - 1;
+            }
+
+            mClasses = classesTemp; /// check
+
+
+        }
+    }
+
+    int getCharacterCode(char ch) {
+        return ch - '$';
+    }
+
+    const size_t MAX_STRING_LENGTH = 400000 + 50;
+    const size_t MAX_ALPHABET_SIZE = 160;
+    size_t mSize;
+    std::string mString;
+
+    std::vector<int> mSuffArray;
+    std::vector<int> mCnt;
+    std::vector<int> mClasses;
+
+
+};
+
+std::vector<int> calculateLCP(SuffixArray& suffixArray) {
+
+
+    for (int i = 0; i < suffixArray.getSize(); ++i) {
+        pos[suffixArray[i]] = i;
+    }
+    int k = 0;
+    for (int i = 0; i < suffixArray.getSize(); ++i) {
+        if (k > 0) {
+            --k;
+        }
+        if (pos[i] == static_cast<int>(suffixArray.getSize() - 1)) {
+            lcp[suffixArray.getSize() - 1] = -1;
+            k = 0;
+        } else {
+            int cur = suffixArray[pos[i] + 1];
+            while (std::max(i + k, cur + k) < suffixArray.getSize() &&
+                   suffixArray.getString()[i + k] == suffixArray.getString()[cur + k]) {
+                ++k;
+            }
+            lcp[pos[i]] = k;
+        }
+
+    }
+    return lcp;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    std::ifstream in("array.in");
+    std::ofstream out("array.out");
+
+    in >> str;
+    str += '$';
+    SuffixArray suffixArray(str);
+    for (int i = 1; i < str.length(); ++i) {
+        out << suffixArray[i] + 1 << " ";
+    }
+    out << std::endl;
+    std::vector<int> lcp = calculateLCP(suffixArray);
+    for (int i = 1; i < str.length() - 1; ++i) {
+        out << lcp[i] << " ";
+    }
+
+    return 0;
+}
